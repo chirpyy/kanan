76ff0c31a77ebffe3b8eb9a07a948d98397a4e7a
QewQew <QewQew@users.noreply.github.com>
2016-07-20 14:39:40 -0700

    Update BitmapFont.js

--- scripts/BitmapFont.js
+++ scripts/BitmapFont.js
@@ -4,8 +4,13 @@
 var bm1 = scan('client.exe', '80 BF 88 00 00 00 00 57 74 0D');
 var bm2 = scan('client.exe', '80 BE 88 00 00 00 00 74 ?? B9 ?? ?? ?? ?? E8 ?? ?? ?? ?? 83 F8 01 75');
 
+if (debug) {
+    send(bm1);
+    send(bm2);
+}
+
 if (bm1 == NULL || bm2 == NULL)
-	console.log('Failed to apply bitmap patch.');
+	send('Failed to apply patch.');
 else
 {
 	patch(bm1.add(8), 0x90);
