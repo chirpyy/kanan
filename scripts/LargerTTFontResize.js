72ef8080fc72d30c1d13a6c6fdde668e183d78c9
Zik <Poshwosh@users.noreply.github.com>
2016-09-16 16:39:42 +0100

    Stub

--- scripts/LargerTTFontResize.js
+++ scripts/LargerTTFontResize.js
@@ -1,5 +1,5 @@
 // Description: 
-// Increase the font size to, close to 14px in size. (created by ???)
+// Increase the font size close to 14px in size. (created by ???)
 // Note that it is incompatible with BitmapFont.js, it will break image searching bots, and there's also some text overflow/overlap on some windows to live with.
 
 var pattern = scan('c7 86 a4 00 00 00 0b 00 00 00 c7');
