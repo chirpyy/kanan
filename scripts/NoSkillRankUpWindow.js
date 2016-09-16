60388f5e7b993c134562ac5d33f9cf6e6cb48017
Zik <Poshwosh@users.noreply.github.com>
2016-09-16 10:02:37 +0100

    Updated mod description

--- scripts/NoSkillRankUpWindow.js
+++ scripts/NoSkillRankUpWindow.js
@@ -1,5 +1,5 @@
 // Description:
-// Skip the white popup window "You have reached Rank 1 Sharp Mind!" after ranking up a skill. (created Step29)
+// Skip the white popup window "You have reached Rank 1 Sharp Mind!" after ranking up a skill. (created by Step29)
 
 var pattern1 = scan('8b f0 33 db 3b f3 75 16 32 c0');
 var pattern2 = scan('e8 ?? ?? ?? ?? 85 c0 0f 84 ?? ?? ?? ?? 8b 10 8b 35');
