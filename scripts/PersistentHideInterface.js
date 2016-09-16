72ae5de6b007a0d3a99e4572894ceb040a239288
Zik <Poshwosh@users.noreply.github.com>
2016-09-16 12:20:53 +0100

    Updated mod description

--- scripts/PersistentHideInterface.js
+++ scripts/PersistentHideInterface.js
@@ -1,4 +1,5 @@
-// While in hide interface mode, using chain combo fighter skills will turn the interface back on. This disables this trigger, persisting the interface to stay hidden. (Created by Rydian)
+// Description: 
+// While in hide interface mode, using chain combo fighter skills will turn the interface back on. This disables this trigger, persisting the interface to stay hidden. (created by Rydian)
 
 var secondchain = scan('83 79 44 01 75 09 6A 00 6A 00 E8');
 patch(secondchain.add(4), 0xEB);
